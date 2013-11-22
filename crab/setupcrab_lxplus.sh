7a95d7b2f556300bca3347ac81cc7677ada2ef02
R. Alex Barbieri <luck@mit.edu>
2013-11-22 19:14:48 +0100

    don't assume CMSSW is defined

--- crab/setupcrab_lxplus.sh
+++ crab/setupcrab_lxplus.sh
@@ -1,12 +1,7 @@
 source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
 voms-proxy-init --valid 168:00 -voms cms 
-#-pwstdin < $HOME/.grid-cert-passphrase
 voms-proxy-info --all
 source /afs/cern.ch/cms/cmsset_default.sh
-#todo to put on afs
-cd /afs/cern.ch/user/r/richard/PRODUCTION/pA_photon_skim_foresting/CMSSW_5_3_8_HI_patch2
-eval `scram runtime -sh`
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
-cd -
-#export STAGE_SVCCLASS=cmscaf 
+echo "WARN: Make sure you have your sourced a CMSSW env before submitting crab jobs."
 
