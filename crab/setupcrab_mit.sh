d51fa494b3743069030b49a5ffef1344597666c3
R. Alex Barbieri <luck@mit.edu>
2014-04-24 17:24:01 -0400

    point to new cvmfs grid env

--- crab/setupcrab_mit.sh
+++ crab/setupcrab_mit.sh
@@ -1,7 +1,6 @@
-source /osg/grid/setup.sh
-source /osg/app/glite/etc/profile.d/grid_env.sh # gLite-UI
+source /cvmfs/grid.cern.ch/3.2.11-1/etc/profile.d/grid-env.sh
+source /cvmfs/cms.cern.ch/crab/crab.sh # CRAB2
 voms-proxy-init --valid 168:00 -voms cms 
 voms-proxy-info --all
-source /cvmfs/cms.cern.ch/crab/crab.sh # CRAB2
 echo "WARN: Make sure you have your sourced a CMSSW env before submitting crab jobs."
 
