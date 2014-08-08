f0af1637bd7367a9ad91d2c0babe607eab4f38e8
R. Alex Barbieri <luck@mit.edu>
2014-08-08 19:42:31 +0200

    crab setup on lxplus for SL6 is now simplified

--- crab/setupcrab_lxplus.sh
+++ crab/setupcrab_lxplus.sh
@@ -1,7 +1,1 @@
-source /afs/cern.ch/cms/LCG/LCG-2/UI/cms_ui_env.sh
-voms-proxy-init --valid 168:00 -voms cms 
-voms-proxy-info --all
-source /afs/cern.ch/cms/cmsset_default.sh
 source /afs/cern.ch/cms/ccs/wm/scripts/Crab/crab.sh
-echo "WARN: Make sure you have your sourced a CMSSW env before submitting crab jobs."
-
